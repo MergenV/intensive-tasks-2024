cea618ed3dac5924e7c93b27f4444223dd04bce7
Mergen <okami2008@yandex.ru>
2024-11-19 21:42:27 +0300

    Решение задачи 1 версия 2

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -30,9 +30,9 @@ public class Task1 {
             return "Вам " + age + " лет";
         } else if (age % 10 == 1) {
             return "Вам " + age + " год";
-        } else if (age % 10 > 1 && age % 10 < 5) {
+        } else if (age % 10 < 5) {
             return "Вам " + age + " года";
-        }  else if (age % 10 > 4 && age % 10 < 10) {
+        }  else if (age % 10 > 4) {
             return "Вам " + age + " лет";
         }
         return "0 лет";
