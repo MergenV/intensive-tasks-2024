6cd4d7af292d93cd8c7429d5f421adef474e3612
Mergen <okami2008@yandex.ru>
2024-11-19 21:53:32 +0300

    Решение задачи 3 версия 2

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -29,10 +29,6 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 }
